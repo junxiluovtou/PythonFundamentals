15c23594cf009fb0296e8c5924cbeb74b9f18c7d
junxiluovtou <137034817+junxiluovtou@users.noreply.github.com>
2025-11-03 00:49:57 +0000

    completed extensions.py

---
+++ 1-Conditionals/extensions/extensions.py
@@ -0,0 +1,11 @@
+extens = {
+    "gif": "image/gif",
+    "jpg": "image/jpeg",
+    "jpeg": "image/jpeg",
+    "png": "image/png",
+    "pdf": "application/pdf",
+    "txt": "text/plain",
+    "zip": "application/zip",
+}
+i = input().split(".")
+print(extens[i[1]])
