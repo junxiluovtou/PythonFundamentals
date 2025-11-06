13b4f99edeab252bc644b94e95fcde043a26aa2f
junxiluovtou <137034817+junxiluovtou@users.noreply.github.com>
2025-11-06 11:56:37 +0000

    completed meal.py

--- 1-Conditionals/meal/meal.py
+++ 1-Conditionals/meal/meal.py
@@ -1,10 +1,22 @@
-def main(): ...
+def main():
+    i = input()
+    convert(i)
 
 
 def convert(time):
     time = time.split(":")
-    time = int(time)
-    time[1] = time[1] / 60
+    # time = int(time)
+    time[1] = int(time[1]) / 60
+    # print(time[0] + time[1])
+    t = int(time[0]) + time[1]
+    if 8 >= t >= 7:
+        print("breakfast")
+    elif 13 >= t >= 12:
+        print("lunch")
+    elif 19 >= t >= 18:
+        print("dinner")
+    else:
+        print("nothing")
 
 
 if __name__ == "__main__":
