e36369a6916b4c94606b72cfb2f273967ef22bd5
junxiluovtou <137034817+junxiluovtou@users.noreply.github.com>
2025-11-16 11:43:27 +0000

    completed grocery.py

--- 3-Exceptions/grocery/grocery.py
+++ 3-Exceptions/grocery/grocery.py
@@ -2,3 +2,14 @@ groceries = {}
 while True:
     try:
         item = input().upper()
+        if item in groceries:
+            groceries[item] += 1
+            # print(item, groceries[item])
+        else:
+            groceries[item] = 1
+            # print(item, groceries[item])
+    except EOFError:
+        # the line below was the problem, it returned a list of keys once sorted
+        grocers = sorted(groceries)
+        for i in grocers:
+            print(groceries[i], i)
